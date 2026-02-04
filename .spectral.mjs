db3b71bf22726dee30f1d4bbed2fcc82611e0546
Dinesh Babhale <dinesh.babhale@smartbear.com>
2026-02-04 10:48:41 +0530

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjMwNDkzMw/spectral.js?branch_slug=main&token=ada9d9de-6898-439d-8fc8-5560e87b6b4e";
+export default { extends: ruleset };
